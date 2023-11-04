e766f297f5a345f769b8bcf42af9493543e2afa2
Pahul <52893813+pahuldeep@users.noreply.github.com>
2023-11-04 22:20:26 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,34 +1,44 @@
-import tkinter as t 
-root = t.Tk()
-root.title("Addition")
-root.geometry("800x400")
-
-lable1=t.Label(root,text="value :")
-lable1.grid(padx=10,pady=10,row=0,column=0)
-
-textbox1=t.Entry(root)
-textbox1.grid(row=0,column=1)
-
-lable2=t.Label(root,text="value :")
-lable2.grid(padx=10,pady=10,row=1,column=0)
-
-textbox2=t.Entry(root)
-textbox2.grid(row=1,column=1)
+import tkinter as tk
 
 def calc():
-    a=textbox1.get()
-    b=textbox2.get()
-    c=int(a)+int(b)
-    textbox3.insert(0,c)
+    # Get the values from the text boxes
+    a = textbox1.get()
+    b = textbox2.get()
 
-lable3=t.Label(root,text="result :")
-lable3.grid(padx=10,pady=10,row=2,column=0)
+    # Calculate the sum
+    c = int(a) + int(b)
 
-textbox3=t.Entry(root)
-textbox3.grid(row=2,column=1)
+    # Display the result in the result text box
+    textbox3.delete(0, "end")
+    textbox3.insert(0, c)
 
-button=t.Button(root,text="sum",command=calc)
-button.grid(row=3,column=1)
+# Create the main window
+root = tk.Tk()
+root.title("Addition Calculator")
+root.geometry("800x400")
 
+# Create labels and text boxes
+label1 = tk.Label(root, text="Value:")
+label1.grid(padx=10, pady=10, row=0, column=0)
 
+textbox1 = tk.Entry(root)
+textbox1.grid(row=0, column=1)
+
+label2 = tk.Label(root, text="Value:")
+label2.grid(padx=10, pady=10, row=1, column=0)
+
+textbox2 = tk.Entry(root)
+textbox2.grid(row=1, column=1)
+
+label3 = tk.Label(root, text="Result:")
+label3.grid(padx=10, pady=10, row=2, column=0)
+
+textbox3 = tk.Entry(root)
+textbox3.grid(row=2, column=1)
+
+# Create a button to calculate the sum
+button = tk.Button(root, text="Calculate Sum", command=calc)
+button.grid(row=3, column=1)
+
+# Start the mainloop
 root.mainloop()
